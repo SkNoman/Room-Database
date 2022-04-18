ed16fc34bc161a51342352b020765b136fcc405d
SkNoman <sknoman77@gmail.com>
2022-04-18 15:34:30 +0600

    first commit

---
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -0,0 +1,81 @@
+package com.example.roomapp.fragments.list
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomapp.R
+import com.example.roomapp.databinding.FragmentListBinding
+import com.example.roomapp.fragments.login
+import com.example.roomapp.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
+    private lateinit var binding:FragmentListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentListBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        // Recyclerview
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
+
+        view.floatingActionButton.setOnClickListener {
+            val text = "Add"
+            val bundle = Bundle()
+            bundle.putString("text",text)
+            Navigation.findNavController(view).navigate(R.id.action_listFragment_to_addFragment, bundle)
+        }
+
+        // Add menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            deleteAllUsers()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteAllUsers()
+            Toast.makeText(
+                requireContext(),
+                "Successfully removed everything",
+                Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.create().show()
+    }
+}
---
+++ app/src/main/java/com/example/roomapp/fragments/login.kt
@@ -0,0 +1,75 @@
+package com.example.roomapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.roomapp.R
+import com.example.roomapp.databinding.FragmentLoginBinding
+import com.example.roomapp.viewmodel.UserViewModel
+
+
+class login : Fragment() {
+
+    private lateinit var mUserViewModel:UserViewModel
+    private lateinit var binding:FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        val view = binding.root
+        binding.btnSignUp.setOnClickListener(View.OnClickListener {
+            val text = "Register"
+            val bundle = Bundle()
+            bundle.putString("text",text)
+            Navigation.findNavController(view).navigate(R.id.action_login_to_addFragment, bundle)
+        })
+
+
+        binding.btnLogin.setOnClickListener(View.OnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            exeLogin(username,password)
+        })
+
+
+        return view
+    }
+
+    private fun exeLogin(username: String,password:String) {
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+
+            var flag = 1
+            for(i in user.indices)
+            {
+                if (user[i].lastName == username && user[i].age.toString() == password)
+                {
+                    findNavController().navigate(R.id.action_login_to_listFragment)
+                    Toast.makeText(requireContext(),"Login Success",Toast.LENGTH_SHORT).show()
+                    flag = 1
+                    break
+                }
+                else
+                {
+                    flag = 0
+                }
+            }
+            if (flag==0)
+            {
+                Toast.makeText(requireContext(),"Wrong Credentials",Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
+
+    }
+
+}
